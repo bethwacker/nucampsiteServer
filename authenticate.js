9b10e30cb825ed99d002772b94856c2f397bdd89
bethwacker <116032081+bethwacker@users.noreply.github.com>
2023-03-10 15:38:01 -0500

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -5,6 +5,7 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const createError = require('http-errors');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config.js');
 
@@ -37,6 +38,7 @@ exports.jwtPassport = passport.use(
         }
     )
 );
+exports.verifyUser = passport.authenticate('jwt', {session: false});
 exports.verifyAdmin = (req, res, next) => {
     if (req.user.admin) {
       // If the user is an admin, allow the request to proceed
@@ -47,6 +49,36 @@ exports.verifyAdmin = (req, res, next) => {
       return next(err);
     }
   };
+
+  exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
+                }
+            });
+        }
+    )
+);
   
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
