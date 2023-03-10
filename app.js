fd74aed3962fecd171cbdb03dd900e13a05ea319
bethwacker <116032081+bethwacker@users.noreply.github.com>
2023-03-10 14:42:49 -0500

    File Upload

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const passport = require('passport');
 const config = require('./config');
 
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 
 const mongoose = require('mongoose');
@@ -29,6 +31,17 @@ connect.then(() => console.log('Connected correctly to server'),
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -43,12 +56,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 // catch 404 and forward to error handler
